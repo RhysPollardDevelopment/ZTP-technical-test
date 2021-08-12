b1ca76edd8954b4a0c5f711c55c01458139b9200
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-08-12 13:44:38 +0100

    Feat: Added post feature which allows creation of total cost and highest rates and displays on the page

--- index/views.py
+++ index/views.py
@@ -8,6 +8,9 @@ class Customer(object):
         self.address = None
         self.meter_number = None
         self.rates = {}
+        self.total = 0
+        self.day_rate = 0
+        self.night_rate = 0
         self.__set_customer_details(object)
 
     def __set_customer_details(self, object):
@@ -27,6 +30,12 @@ class Customer(object):
 
 
 def index(request):
+
+    # Variables for cost calculations
+    total_cost = None
+    day_rate = None
+    night_rate = None
+
     data = get_data("EnergyConsumptionDetail_updated(1470).xlsx")
 
     keys = data.keys()
@@ -49,22 +58,52 @@ def index(request):
         customers.append(customer)
 
     if request.method == "POST":
-        energy_rates = []
+        energy_rates = {}
         for cost in rate_prices:
             if cost[0] == "Rate Name":
                 continue
             else:
-                energy_rates.append(cost)
-        print(energy_rates)
+                energy_rates[cost[0]] = cost[1]
+
+        highest_total = 0
+        highest_day = 0
+        highest_night = 0
 
         for customer in customers:
-            for energy_cost in energy_rates:
-                if energy
-                
+            rates = customer.rates.keys()
+
+            for rate in rates:
+                if rate in energy_rates:
+                    length = len(customer.rates[rate])
+                    usage = (
+                        customer.rates[rate][length - 1]
+                        - customer.rates[rate][0]
+                    )
+                    cost = usage * energy_rates[rate]
+                    if rate == "Day Rate":
+                        customer.day_rate = cost
+                    elif rate == "Night Rate":
+                        customer.night_rate = cost
+                    customer.total += cost
+
+            if customer.total > highest_total:
+                highest_total = customer.total
+                total_cost = customer
+
+            if customer.day_rate > highest_day:
+                highest_day = customer.day_rate
+                day_rate = customer
+
+            if customer.night_rate > highest_night:
+                highest_night = customer.night_rate
+                night_rate = customer
 
     context = {
         "customers": customers,
         "rate_types": rate_types,
+        "total_cost": total_cost,
+        "day_rate": day_rate,
+        "night_rate": night_rate,
     }
     template = "index/customers.html"
     return render(request, template, context)
